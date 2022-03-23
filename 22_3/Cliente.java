fc7a94265544412fe16332d1ae59cb9777841570
aabdala31 <81708968+aabdala31@users.noreply.github.com>
2022-03-23 10:37:11 -0300

    Add files via upload

---
+++ 22_3/Cliente.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private float edad;
+    private ArrayList<Libro> librosComprados;
+
+
+    public Cliente(int id, String nombre, float edad, ArrayList<Libro> librosComprados) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.librosComprados = librosComprados;
+    }
+
+    public Cliente(ArrayList<Libro> librosComprados){
+        this.id = 2;
+        this.nombre = "tomate";
+        this.edad = 12;
+        this.librosComprados = librosComprados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getEdad() {
+        return edad;
+    }
+
+    public void setEdad(float edad) {
+        this.edad = edad;
+    }
+
+    public int getSize(){
+        return librosComprados.size();
+    }
+
+}
